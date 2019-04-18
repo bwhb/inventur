41240c3459da3988cf42f830b16f23b63de866db
bw <bw@yaq1.net>
2019-04-18 07:45:29 +0200

    Verbesserter Import

---
+++ addlok2.php
@@ -0,0 +1,79 @@
+<?php
+ini_set('memory_limit', '1024M'); // or you could use 1G
+ini_set('display_errors','Off');ini_set('error_reporting', 0 );
+
+require __DIR__ . '/vendor/autoload.php';
+
+
+use pear\file_marc\File;
+use  PHPOnCouch\CouchClient; //The CouchDB client object
+
+$client = new CouchClient('http://127.0.0.1:5984', 'marc21');
+$journals = new File_MARC('data/marc21/051-lok.mrc');
+$os = [];
+$ppns = [];
+echo "\nVerarbeite Lokaldaten";
+while ($record = $journals->next()) {
+	
+	foreach($record->getFields('852') as $v) forEach($v->getSubfields("c") as $sig) //echo $sig."\n";
+    
+
+}
+while ($record = $journals->next()) {
+	
+	foreach($record->getFields('852') as $v) forEach($v->getSubfields("c") as $sig) //echo $sig."\n";
+    
+
+}
+//    if((preg_match("/preu|kultur/i",$record->getField('852', true)->getSubfield('z')) && preg_match(!"/reichsgericht/i",$record->getField('852', true)->getSubfield('z')))||preg_match("/^pr|^ol/i",$record->getField('935', true))||(!preg_match("/(par|ent|ads|zsn|nib|np|a 25|8\+|4\+|2\+)/i",$sig))){
+    if(!preg_match("/reichsgericht|volksgericht/i",$record->getField('852', true)->getSubfield('z')||!preg_match("/(par|ent|ads|zsn|nib|np|a 25|8\+|4\+|2\+)/i",$sig) && !$record->getField('935', true))&&((preg_match("/preu|kultur/i",$record->getField('852', true)->getSubfield('z')))||preg_match("/^pr|^ol/i",$record->getField('935', true)))){
+		$i++;
+		unset ($o);
+		if ($record->getField('004', true)){
+			$o->ppn = preg_replace("/\d{3}\s+(.*)/","$1",$record->getField('004', true)->__toString());
+		}
+		if ($record->getField('866', true)&&$record->getField('866', true)->getSubfield('a')){
+			$o->seq = preg_replace("/.*?_a(.*?)_?.*?/","$1",$record->getField('866')->__toString());
+			$o->seq = preg_replace("/(.*?)_.*/","$1",$o->seq);
+		}
+
+		$o->sig =preg_replace("/\[c\]:\s(.*)/","$1",$sig);  
+		$o->sig =preg_replace("/^[\-\#\+]*(.*)/","$1",$o->sig);  
+		  
+		while(preg_match("/(.*\b)(\d{1,3})(\b.*)/",$o->sig)){
+			$o->sig =preg_replace_callback("/(.*\b)(\d{1,3})(\b.*)/",function($t){
+				$t[2]= str_pad($t[2], 4, "0", STR_PAD_LEFT);
+				return $t[1].$t[2].$t[3];
+			},$o->sig);
+		}  
+		$o->sig =preg_replace("/^0+([1-9])/","$1",$o->sig);  
+		//preg_replace("/\d{3}\s+1 _c([^\n]*?)\n.*/","$1",$record->getFields('852',true)[2]);
+		$o->tb = preg_replace("/\[\w\]:\s+(.*)/","$1",$record->getField('852', true)->getSubfield('z'));
+		if ($record->getField('935', true))$o->tbkz = preg_replace("/\d{3}\s+_a(.{2}).*/","$1",$record->getField('935', true));
+		if(preg_match("/preu/i",$record->getField('852', true)->getSubfield('z')))$o->tbkz = "pr";
+		if(preg_match("/kultur/i",$record->getField('852', true)->getSubfield('z')))$o->tbkz = "ol";
+		if (!$o->tbkz or empty($o->tbkz)) {
+			$o->tbkz = "pr";
+
+		}
+		//print_r($o);
+
+		//if ($o->seq)die($record->__toString());
+		/*
+		//$o =json_decode(Zend\Xml2Json\Xml2Json::fromXml($record->toXML(), true));
+    	*/
+    	$o->_id = preg_replace("/\d{3}\s+(.*)/","A$1",$record->getField('004', true)->__toString());
+    	$ppns[] = preg_replace("/\d{3}\s+(.*)/","$1",$record->getField('004', true)->__toString());
+    	array_push($os, $o);
+    	if($i==1000){
+    		echo ".";
+			$client->storeDocs($os);
+			$os = [];
+			$i =0;
+		}
+	}
+
+}
+$client->storeDocs($os);
+file_put_contents("ppns.json", json_encode($ppns))
+?>
--- marcreader.py
+++ marcreader.py
@@ -13,45 +13,45 @@ couch = couchdb.Server('http://localhost:5984/')
 db = couch['ppn']
 docs = []
 i = 0
-# print("Verarbeite Titeldaten")
-# with open('data/marc21/051-tit.mrc', 'rb') as fh:
-#     reader = MARCReader(fh)
-#     for record in reader:
-#         dbEntry = {}
-#         aufl =''
-#         if record['100']:
-#             aut = record['100']['a'] 
-#         dbEntry['_id'] = record['001'].format_field()
+print("Verarbeite Titeldaten")
+with open('data/marc21/051-tit.mrc', 'rb') as fh:
+    reader = MARCReader(fh)
+    for record in reader:
+        dbEntry = {}
+        aufl =''
+        if record['100']:
+            aut = record['100']['a'] 
+        dbEntry['_id'] = record['001'].format_field()
         
-#         if record['250']:
-#             dbEntry['aufl'] = record['250']['a']
-#         else:
-#             dbEntry['aufl'] = ""
+        if record['250']:
+            dbEntry['aufl'] = record['250']['a']
+        else:
+            dbEntry['aufl'] = ""
         
-#         if record['362']:
-#             dbEntry['seq'] = record['362']['a']
-#         else:
-#             dbEntry['seq'] = ""
+        if record['362']:
+            dbEntry['seq'] = record['362']['a']
+        else:
+            dbEntry['seq'] = ""
 
-#         dbEntry['jahr'] = record.pubyear()
-#         dbEntry['titel'] = record.title()
+        dbEntry['jahr'] = record.pubyear()
+        dbEntry['titel'] = record.title()
         
-#         if record['100']:
-#             dbEntry['aut'] = record['100']['a']
-#         else:
-#             dbEntry['aut'] = ""
+        if record['100']:
+            dbEntry['aut'] = record['100']['a']
+        else:
+            dbEntry['aut'] = ""
 
-#         if record['773'] and record['245']:
-#             dbEntry['gtitel'] = ' '.join([xstr(record['245']['a']),xstr(record['245']['n']),xstr(record['773']['q'])])
+        if record['773'] and record['245']:
+            dbEntry['gtitel'] = ' '.join([xstr(record['245']['a']),xstr(record['245']['n']),xstr(record['773']['q'])])
        
-#         if record['773'] and record['490']:
-#             dbEntry['gtitel'] = ' '.join([xstr(record['490']['a']),xstr(record['490']['v']),xstr(record['773']['q'])])
-#         docs.append(dbEntry)
-#         if i == 3000:
-#             db.update((docs))
-#             docs = []
-#             i=0
-#         i = i + 1
+        if record['773'] and record['490']:
+            dbEntry['gtitel'] = ' '.join([xstr(record['490']['a']),xstr(record['490']['v']),xstr(record['773']['q'])])
+        docs.append(dbEntry)
+        if i == 3000:
+            db.update((docs))
+            docs = []
+            i=0
+        i = i + 1
         
 
 print("Verarbeite Lokaldaten")
@@ -76,37 +76,39 @@ with open('data/marc21/051-lok.mrc', 'rb') as fh:
                 dbEntry['sig'] = re.findall(r'^[\-\#\+]*(.*)', f['c'])
                 dbEntry['sig'] = dbEntry['sig'][0]
                 dbEntry['sig'] = re.sub(r'(.*?\b)(\d{1,4})(\b.*)',signieren,dbEntry['sig'])
+                #print(dbEntry['sig'])
                 #print (dbEntry['sig'])
+        if "sig" in dbEntry:
+            if re.search('Teilbestand.*(Preu|Oberlandeskultur)',record['852'].format_field()) or (record['935'] and re.search('pr|ol',record['935'].format_field()) or not record['935']) :
+                if not re.search('reichs',record['852'].format_field(), re.IGNORECASE) and not re.search(r'(par|ent|ads|zsn|nib|np|a 25|8\+|4\+|2\+)',dbEntry['sig'], re.IGNORECASE):
+                    if re.search('Oberlandeskultur',record['852'].format_field()):
+                        dbEntry['tbkz'] = "ol"
+                        db = couch['ppn']
+                        doc = db.get(record['004'].format_field())
+                        if doc:
+                            dbEntry.update(doc)
+                            dbEntry['_id'] = record['001'].format_field()
+                            del dbEntry['_rev']
+                            ol.append(dbEntry)
+                        if i % 1000 == 0:
+                            db = couch['ol']
+                            db.update(ol)
+                            ol = []
 
-        if re.search('Teilbestand.*(Preu|Oberlandeskultur)',record['852'].format_field()) or (record['935'] and re.search('pr|ol',record['935'].format_field()) or not record['935']) :
-            if not re.search('reichs',record['852'].format_field(), re.IGNORECASE) or not re.search(r'(par|ent|ads|zsn|nib|np|a 25|8\+|4\+|2\+)',xstr(dbEntry['sig']), re.IGNORECASE):
-                if re.search('Oberlandeskultur',record['852'].format_field()):
-                    dbEntry['tbkz'] = "ol"
-                    doc = db.get(record['004'].format_field())
-                    if doc:
-                        dbEntry.update(doc)
-                        dbEntry['_id'] = record['001'].format_field()
-                    ol.append(dbEntry)
-                    if i % 1000 == 0:
-                        #print(ol)
-                        db = couch['ol']
-                        db.update(ol)
-                        ol = []
+                    else:
+                        dbEntry['tbkz'] = "pr"
+                        db = couch['ppn']
+                        doc = db.get(record['004'].format_field())
+                        if doc:
+                            dbEntry.update(doc)
+                            dbEntry['_id'] = record['001'].format_field()
+                            del dbEntry['_rev']
+                            pr.append(dbEntry)
+                        if i % 1000 == 0:
+                            db = couch['pr']
+                            db.update(pr)
+                            #print(pr)
+                            pr = []
 
-                else:
-                    dbEntry['tbkz'] = "pr"
-                    doc = db.get(record['004'].format_field())
-                    if doc:
-                        dbEntry.update(doc)
-                        dbEntry['_id'] = record['001'].format_field()
-                    pr.append(dbEntry)
-                    if i % 1000 == 0:
-                        #print(pr)
-                        db = couch['pr']
-                        db.update(pr)
-                        pr = []
-
-                #print(dbEntry)
-
-            else: 
-                pass
+                else: 
+                    pass
